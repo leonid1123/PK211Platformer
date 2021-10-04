aa5eb105d40d6edaa478a0797106d1b1ad8e9a84
Leo <47742726+leonid1123@users.noreply.github.com>
2021-10-04 12:28:57 +0300

    bottle count

--- Assets/Scripts/bottleCont.cs
+++ Assets/Scripts/bottleCont.cs
@@ -4,19 +4,13 @@ using UnityEngine;
 
 public class bottleCont : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Destroy(gameObject);
+    }
+    private void OnDestroy()
+    {
+        GameObject.Find("Player").GetComponent<tepMove>().BottleAdd();
     }
 }
--- Assets/Scripts/tepMove.cs
+++ Assets/Scripts/tepMove.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;//для интерфейса пользователя
 
 public class tepMove : MonoBehaviour
 {
@@ -9,10 +10,15 @@ public class tepMove : MonoBehaviour
     float speed = 25f;
     bool jump = false;
 
+    private int BottleCount;//считать бутылки
+    public Text ScoreText;//писать текст
+
     // Update is called once per frame
     void Update()
     {
+        ScoreText.text = "Score:" + BottleCount.ToString();
         hMove = Input.GetAxisRaw("Horizontal")*speed;
+        Debug.Log(hMove);
         if (Input.GetButtonDown("Jump") )
         {
             jump = true;
@@ -22,5 +28,9 @@ public class tepMove : MonoBehaviour
     {
         controller.Move(hMove*Time.fixedDeltaTime,false,jump);
         jump = false;
+    }
+    public void BottleAdd() //метод для увеличения количества бутылок
+    {
+        BottleCount += 1;
     }
 }
