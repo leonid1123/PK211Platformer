ce9d42c8eb0bd9dc0e5e396613fd63c8185b54bf
Leo <47742726+leonid1123@users.noreply.github.com>
2021-10-18 13:58:52 +0300

    cast

--- Assets/Scripts/newPlayer/NewPlayerCont.cs
+++ Assets/Scripts/newPlayer/NewPlayerCont.cs
@@ -6,11 +6,15 @@ public class NewPlayerCont : MonoBehaviour
 {
     private Rigidbody2D rb2;
     private Animator PlayerAnim;
+    private AnimatorStateInfo atkAllow;
 
     private float hMove = 0;
     private float speed = 3f;
 
     private bool toRight = true;
+
+    private bool canAtk = true;
+
 
 
     void Start()
@@ -20,17 +24,29 @@ public class NewPlayerCont : MonoBehaviour
     }
     void Update()
     {
-        if (!PlayerAnim.GetCurrentAnimatorStateInfo(0).IsName("PlayerAttack1"))
+        atkAllow = PlayerAnim.GetCurrentAnimatorStateInfo(0);
+        if (!atkAllow.IsName("PlayerAttack1") && !atkAllow.IsName("PlayerCast"))
         {
             hMove = Input.GetAxisRaw("Horizontal");
             PlayerAnim.SetFloat("moveSpeed", Mathf.Abs(hMove));
             Movement(hMove, speed);
         }
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1") && canAtk)
         {
+            canAtk = false;
             PlayerAnim.SetTrigger("atk1");
-            rb2.velocity = new Vector2(0, 0);
+            rb2.velocity = new Vector2(0, 0); //потом тут будут косяки!!!
+            Invoke("CanAttack",0.5f);
         }
+        if (Input.GetButtonDown("Fire2") && canAtk)
+        {
+            canAtk = false;
+            PlayerAnim.SetTrigger("cast");
+            rb2.velocity = new Vector2(0, 0); //потом тут будут косяки!!!
+            Invoke("CanAttack", 0.5f);
+        }
+
+
     }
     void Movement(float move, float spd)
     {
@@ -49,5 +65,8 @@ public class NewPlayerCont : MonoBehaviour
         toRight = !toRight;
         transform.Rotate(0, 180f, 0);
     }
-
+    void CanAttack()
+    {
+        canAtk = true;
+    }
 }
